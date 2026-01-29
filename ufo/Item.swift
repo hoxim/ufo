14db8ad54233c50399becc7f949d2eff5c06b8d3
hoxim <mr@hoxim.com>
2026-01-29 15:38:55 +0100

    Initial Commit

---
+++ ufo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ufo
+//
+//  Created by Marcin Ryzko on 29/01/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
